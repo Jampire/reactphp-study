5fbfcb0adac513f1f1fc5c02e61df1179b193c9f
Dzianis Kotau <jampire.blr@gmail.com>
2019-10-07 02:05:16 +0300

    Streams

--- read.php
+++ read.php
@@ -2,5 +2,19 @@
 
 require 'vendor/autoload.php';
 
-use React\EventLoop\Factory;
+use React\EventLoop\Factory as LoopFactory;
+use React\Stream\ReadableResourceStream;
 
+$loop = LoopFactory::create();
+$stream = new ReadableResourceStream(fopen('composer.json', 'r'), $loop, 1);
+$stream->on('data', static function($data) use ($stream, $loop) {
+    echo $data, PHP_EOL;
+    $stream->pause();
+
+    $loop->addTimer(1, static function() use ($stream) {
+        $stream->resume();
+    });
+
+});
+
+$loop->run();
